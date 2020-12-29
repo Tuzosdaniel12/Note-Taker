3ccd670295583f7370e44e465e6c9fbf01c6b179
daniel <danielsoledad@gmail.com>
2020-12-28 20:18:24 -0800

    update to handle bars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,8 +5,12 @@ const PUBLIC_DIR = path.resolve(__dirname, "../public");
 module.exports = function(app) {
 // HTML Routes
 
-app.get('/notes', (req, res) => res.sendFile(path.join(PUBLIC_DIR, 'notes.html')))
+app.get('/', (req, res)=>{
+    res.render("index");
+})
 
-app.get('*', (req, res) => res.sendFile(path.join(PUBLIC_DIR, 'index.html')))
+app.get('/notes', (req, res)=>{
+    res.render("notes");
+})
 
 }
